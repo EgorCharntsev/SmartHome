e7830993268fa38e8b28670a2ed7e15fe6365347
EgorCharntsev <charncev200447@gmail.com>
2023-11-10 23:01:18 +0300

    added insert script into table Sensors

---
+++ src/main/java/net/util/database/sql/V3.17__insertValuesIntoSensors.sql
@@ -0,0 +1,24 @@
+INSERT INTO
+    sensors (status, commission_date, type_id)
+VALUES
+    ('true','2023-11-10 22:43:34.588000','2');
+
+INSERT INTO
+    sensors (status, commission_date, type_id)
+VALUES
+    ('true','2023-11-10 22:44:34.588000','3');
+
+INSERT INTO
+    sensors (status, commission_date, type_id)
+VALUES
+    ('true','2023-11-10 22:45:34.588000','4');
+
+INSERT INTO
+    sensors (status, commission_date, type_id)
+VALUES
+    ('true','2023-11-10 22:46:34.588000','5');
+
+INSERT INTO
+    sensors (status, commission_date, type_id)
+VALUES
+    ('true','2023-11-10 22:47:34.588000','6');
